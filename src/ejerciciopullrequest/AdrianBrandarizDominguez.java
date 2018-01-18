c082c2ab9fadce43f6763c0c76edb398865e1444
Hansen <Hansen@DESKTOP-P1ST9AA>
2018-01-18 09:10:24 +0100

    Nueva clase

---
+++ src/ejerciciopullrequest/AdrianBrandarizDominguez.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciopullrequest;
+
+/**
+ *
+ * @author Hansen
+ */
+public class AdrianBrandarizDominguez {
+    
+    public void damianCalifica(){
+        System.out.println("Alumno: Adrian Brandariz Dominguez\nNota: 10");
+    }
+}
