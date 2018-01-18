2a95176b1106b7a6d5fa3e89e5605e6e39bcbd5a
jose <jose@portatil.mshome.net>
2018-01-18 09:18:56 +0100

    crear clase co meu nome

---
+++ src/ejerciciopullrequest/JoseDominguezMartinan.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciopullrequest;
+
+/**
+ *
+ * @author jose
+ */
+public class JoseDominguezMartinan {
+    public void exercicio(){
+    System.out.println("exercicio de jose dominguez martiñan");
+    }
+    
+}
