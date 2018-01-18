cb126f29954beadcb11c0cc772fd3557bca91817
femio23 <chiscazo.paco@gmail.com>
2018-01-18 09:02:03 +0100

    PullRequest N6325

---
+++ src/ejerciciopullrequest/N6325.java
@@ -0,0 +1,16 @@
+
+package ejerciciopullrequest;
+
+/**
+ *
+ * @author femio23
+ * alias:Francisco Romay Méndez
+ */
+public class N6325 {
+
+    public N6325() {
+    }
+    public void saludar(){
+        System.out.println("Bos dias, saudate unha modificación de Francisco Romay Méndez.");
+    }
+}
