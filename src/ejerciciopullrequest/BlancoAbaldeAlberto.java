71f4373ca9c42e4122e6e62e1c2e249990dbecfd
ablancoabalde <ablancoabalde@victoria6.danielcastelao.org>
2018-01-18 08:58:28 +0100

    Branch nueva

---
+++ src/ejerciciopullrequest/BlancoAbaldeAlberto.java
@@ -0,0 +1,13 @@
+
+package ejerciciopullrequest;
+
+/**
+ *
+ * @author ablancoabalde
+ */
+public class BlancoAbaldeAlberto {
+    
+    public void hola(){
+        System.out.println("Hola");
+    }
+}
