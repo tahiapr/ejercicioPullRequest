e8be84ee39dac7f4c4d061dcb89e1af3c014d231
tperezrodriguez <tperezrodriguez@victoria5.danielcastelao.org>
2018-01-25 11:23:17 +0100

    Añadida rama Tahía con class y saludo

---
+++ src/ejerciciopullrequest/TahiaPerezRodriguez.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciopullrequest;
+
+/**
+ *
+ * @author tperezrodriguez
+ */
+public class TahiaPerezRodriguez {
+    public void saludoTahia(){
+        System.out.println("Hola, carambola");
+    }
+}
